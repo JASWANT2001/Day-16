86ec550a646d4c686e8652932fbdf71ba6d8d3d9
JASWANT2001 <k.jaswant2305@gmail.com>
2023-10-16 20:14:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+//CALLBACK HELL FUNCTION
+
+let count = 11;
+setTimeout(() =>{
+    count--;
+    console.log(count);
+    var screen = document.querySelector('#countdown');
+    screen.innerText = count;
+    setTimeout(() =>{
+        count--;
+        console.log(count);
+        var screen = document.querySelector('#countdown');
+        screen.innerText = count;
+        setTimeout(() =>{
+            count--;
+            console.log(count);
+            var screen = document.querySelector('#countdown');
+            screen.innerText = count;
+            setTimeout(() =>{
+                count--;
+                console.log(count);
+                var screen = document.querySelector('#countdown');
+                screen.innerText = count;
+                setTimeout(() =>{
+                    count--;
+                    console.log(count);
+                    var screen = document.querySelector('#countdown');
+                    screen.innerText = count;
+                    setTimeout(() =>{
+                        count--;
+                        console.log(count);
+                        var screen = document.querySelector('#countdown');
+                        screen.innerText = count;
+                        setTimeout(() =>{
+                            count--;
+                            console.log(count);
+                            var screen = document.querySelector('#countdown');
+                            screen.innerText = count;
+                            setTimeout(() =>{
+                                count--;
+                                console.log(count);
+                                var screen = document.querySelector('#countdown');
+                                screen.innerText = count;
+                                setTimeout(() =>{
+                                    count--;
+                                    console.log(count);
+                                    var screen = document.querySelector('#countdown');
+                                    screen.innerText = count;
+                                    setTimeout(() =>{
+                                        count--;
+                                        console.log(count);
+                                        var screen = document.querySelector('#countdown');
+                                        screen.innerText = count;
+                                        setTimeout(() =>{
+                                            count--;
+
+                                            console.log(count);
+                                            var screen = document.querySelector('#countdown');
+                                            screen.innerText = count;
+                                            clearTimeout();
+                                            console.log("HAPPY INDEPENDENCE DAY");
+                                            var screen = document.querySelector('#countdown');
+                                            screen.innerText = "HAPPY INDEPENDENCE DAY!!!"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
